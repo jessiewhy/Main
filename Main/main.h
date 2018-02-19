a884566a65fe08a08bf8805a1ef67e4e632721cd
Jessie Huang <jessiemh@ufl.edu>
2018-02-19 17:47:05 -0500

    adding final project to git repository

---
+++ Main/main.h
@@ -0,0 +1,22 @@
+#ifndef PA1_H
+#define PA1_H
+
+
+class pa1
+{
+public:
+	pa1();
+	void MagicSquare(int **matrix, int n);
+	void MyMagicSquare(int **matrix, int n);
+	void transpose(int **matrix, int n);
+	void flipRows(int **matrix, int n);
+	void flipColumns(int **matrix, int n);
+	void CheckSum(int **matrix, int n);
+	void PrintMagicSquare(int **matrix, int n);
+
+protected:
+
+private:
+};
+
+#endif // PA1_H
